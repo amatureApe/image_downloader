4b30d4ce0d3b0cb35c9e4831a410a88d083af495
amatureApe <amatureApe@gmail.com>
2024-07-24 16:40:18 +0800

    download image

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use error_chain::error_chain;
+use std::io::copy;
+use std::fs::File;
+use std::path::PathBuf;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let project_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));  // Gets the directory of your Cargo.toml
+    let src_dir = project_dir.join("src");  // Path to the src directory
+
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("file to download: '{}'", fname);
+        let fname = src_dir.join(fname);
+        println!("will be located under: '{:?}'", fname);
+        File::create(fname)?
+    };
+    let content =  response.text().await?;
+    copy(&mut content.as_bytes(), &mut dest)?;
+    Ok(())
+}
